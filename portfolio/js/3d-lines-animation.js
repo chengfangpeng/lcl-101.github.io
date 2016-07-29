f71a2321fa5ee633faf4004656a847dcf1449a5e
lichenglong <554675188@qq.com>
2016-07-29 10:40:01 +0800

    lcl

--- portfolio/js/3d-lines-animation.js
+++ portfolio/js/3d-lines-animation.js
@@ -135,12 +135,12 @@ var mouseX = 0, mouseY = 0,
                 setInterval(function(){
                     aa++;
                     if(aa>=1000){
-                        mouseX = (aa - windowHalfX) * 0.7;
-                        mouseY = (aa - windowHalfY) * 0.7;
-                        aa=100;
-                    }else {
                         mouseX = (aa + windowHalfX) * 0.7;
                         mouseY = (aa + windowHalfY) * 0.7;
+                        aa=100;
+                    }else {
+                        mouseX = (aa - windowHalfX) * 0.7;
+                        mouseY = (aa - windowHalfY) * 0.7;
                     }
                 },50);
                 //if ( event.touches.length > 1 ) {
